6133462a41fa49478e9ebf60463e28e5ec5baefb
luckylion <noderam@gmail.com>
2017-05-10 14:16:03 +0800

    Create yk.js

---
+++ yk.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name         yk
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://vip.youku.com/vips/accountCard.html
+// @require      http://cdn.bootcss.com/jquery/1.7.2/jquery.min.js
+// @require      http://speed-bg.qiniudn.com/jquery.timers.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+kaiguan();
+$('body').everyTime('1800s','A',function(){
+    kaiguan();
+},0,true);
+
+    // Your code here...
+})();
+
+function kaiguan()
+{
+	$("body").html('');
+	$.getJSON("http://vip.youku.com/?c=ajax&a=ajax_speedup_service_switch",function(data){
+		$("body").append(ThisTime()+' '+data.msg+'<br/>');
+		if(data.result.state==2)
+		{
+			kaiguan();
+		}
+		else{
+			tisu();
+		}
+	});
+}
+
+function tisu()
+{
+	$.getJSON("http://vip.youku.com/?c=ajax&a=ajax_do_speed_up",function(data){
+		$("body").append(ThisTime()+' '+data.msg+'<br/>');
+	});
+}
+
+
+function ThisTime()
+{
+	var date = new Date(new Date().getTime());
+	Y = date.getFullYear() + '-';
+	M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+	D = date.getDate() + ' ';
+	h = date.getHours() + ':';
+	m = date.getMinutes() + ':';
+	s = date.getSeconds();
+	return Y+M+D+h+m+s;
+}
